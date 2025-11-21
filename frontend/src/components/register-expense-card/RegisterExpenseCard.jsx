5e1ffa6e6d5b61679c1985aa868a1aeb98a09069
andregarcia0412 <andregarcia0412@gmail.com>
2025-11-21 13:24:47 -0300

    feat(frontend): add register expense card animation and backdrop blur

--- frontend/src/components/register-expense-card/RegisterExpenseCard.jsx
+++ frontend/src/components/register-expense-card/RegisterExpenseCard.jsx
@@ -9,14 +9,8 @@ const RegisterExpenseCard = ({ btnOnClick, onClose }) => {
   const categoryInput = React.useRef();
   const amountInput = React.useRef();
   const dateInput = React.useRef();
-  
-  const dateOptions = {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  };
 
-  let today = new Date().toISOString().split("T")[0]
+  let today = new Date().toISOString().split("T")[0];
 
   const [errorMessage, setErrorMessage] = React.useState("");
 
@@ -104,68 +98,75 @@ const RegisterExpenseCard = ({ btnOnClick, onClose }) => {
   }
 
   return (
-    <div className="register-expense-container">
-      <div className="register-expense-header">
-        <h1>Register New Expense</h1>
-        <button onClick={onClose}>
-          <img src={Close} />
+    <div className="backdrop">
+      <div className="register-expense-container">
+        <div className="register-expense-header">
+          <h1>Register New Expense</h1>
+          <button onClick={onClose}>
+            <img src={Close} />
+          </button>
+        </div>
+        <div className="register-input">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            ref={nameInput}
+            placeholder="What is the expense?"
+          />
+        </div>
+        <div className="register-input">
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="text"
+            name="amount"
+            ref={amountInput}
+            placeholder="0.00"
+          />
+        </div>
+        <div className="register-input">
+          <label htmlFor="category">Category</label>
+          <CategorySelect
+            options={selectOptions}
+            name="category"
+            ref={categoryInput}
+          />
+        </div>
+        <div className="register-input">
+          <label htmlFor="description">Description (optional)</label>
+          <input
+            type="text"
+            name="description"
+            ref={descriptionInput}
+            placeholder="What was this expense for?"
+          />
+        </div>
+        <div className="register-input">
+          <label htmlFor="date">Date</label>
+          <input type="date" name="date" defaultValue={today} ref={dateInput} />
+        </div>
+
+        <p style={{ color: "red" }}>{errorMessage}</p>
+
+        <button
+          onClick={() => {
+            const result = validateFields();
+            if (result.ok) {
+              btnOnClick(name, category, description, amount, date);
+              return;
+            } else if (result.culprit != categoryInput.current) {
+              result.culprit.classList.add("shake");
+              setTimeout(() => {
+                result.culprit.classList.remove("shake");
+              }, 500);
+
+              setErrorMessage(result.message);
+            }
+          }}
+        >
+          Add Expense
         </button>
       </div>
-      <div className="register-input">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          ref={nameInput}
-          placeholder="What is the expense?"
-        />
-      </div>
-      <div className="register-input">
-        <label htmlFor="amount">Amount</label>
-        <input type="text" name="amount" ref={amountInput} placeholder="0.00" />
-      </div>
-      <div className="register-input">
-        <label htmlFor="category">Category</label>
-        <CategorySelect
-          options={selectOptions}
-          name="category"
-          ref={categoryInput}
-        />
-      </div>
-      <div className="register-input">
-        <label htmlFor="description">Description (optional)</label>
-        <input
-          type="text"
-          name="description"
-          ref={descriptionInput}
-          placeholder="What was this expense for?"
-        />
-      </div>
-      <div className="register-input">
-        <label htmlFor="date">Date</label>
-        <input type="date" name="date" defaultValue={today} ref={dateInput} />
-      </div>
-
-      <p style={{ color: "red" }}>{errorMessage}</p>
-
-      <button
-        onClick={() => {
-          const result = validateFields();
-          if (result.ok) {
-            btnOnClick(name, category, description, amount, date);
-            return;
-          } else if (result.culprit != categoryInput.current) {
-            result.culprit.classList.add("shake");
-            setTimeout(() => {
-              result.culprit.classList.remove("shake");
-            }, 500);
-
-            setErrorMessage(result.message);
-          }
-        }}
-      >
-        Add Expense
-      </button>
     </div>
   );
 };
