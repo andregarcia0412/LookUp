09ecff10e24375088601c8eaee72bf4429018619
andregarcia0412 <andregarcia0412@gmail.com>
2025-11-25 18:14:39 -0300

    feat(mobile): start mobile with api communication

---
+++ mobile/app/about.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+const About = () => {
+    <View>
+        <Text>About</Text>
+    </View>
+}
+
+export default About
---
+++ mobile/service/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+api.interceptors.request.use(async (config) => {
+  const user_data = await AsyncStorage.getItem("user_data");
+  if (user_data) {
+    config.headers.Authorization = `Bearer ${JSON.parse(user_data).token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status == 401) {
+      await AsyncStorage.removeItem("user_data");
+      //window.location.href = "/auth"; colocar outra pagina
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
