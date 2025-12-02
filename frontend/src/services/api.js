0639d12a2784aade5a3c5ad4e9a3c29bccdf0b57
andregarcia0412 <andregarcia0412@gmail.com>
2025-12-02 16:26:48 -0300

    refactor(frontend): change api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "desafiofullstack-production.up.railway.app:3000",
 });
 
 api.interceptors.request.use((config) => {
